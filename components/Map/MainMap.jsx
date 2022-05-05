995d6c9b18486e14339ca8c379d5eeb5edc48032
Ashirmatov18 <beksultanashirmatov@gmail.com>
2022-05-05 07:33:57 +0600

    map

--- components/Map/MainMap.jsx
+++ components/Map/MainMap.jsx
@@ -1,19 +1,41 @@
 import RadioSVGMap from "./RadioSvg";
 import KgMap from "./map";
-import { useState } from "react";
 import styles from "../../styles/maps.module.css";
 import Image from "next/image";
 import kg from "../../img/kg.png";
 import rus from "../../img/ru.png";
 import ru from "../../img/russian.png";
+import React, { useState, useEffect, useRef } from "react";
+import Footer from "../Footer";
+
+let useClickOutside = (handler) => {
+  let domNode = useRef();
+
+  useEffect(() => {
+    let maybeHandler = (event) => {
+      if (!domNode.current.contains(event.target)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("mousedown", maybeHandler);
+
+    return () => {
+      document.removeEventListener("mousedown", maybeHandler);
+    };
+  });
+
+  return domNode;
+};
 
 const options = [
   {
     id: "issyk",
     data: [
       {
-        street: "test issyk street",
-        phone: "test issyk phone",
+        street:
+          "Г. Каракол, ул. Жамансариева 188/3, пер. Алдашева (ор-р «Актилек+») ",
+        phone: "+996501805151 ",
         title: "Иссык-Куль",
       },
       {
@@ -33,26 +55,119 @@ const options = [
       },
     ],
   },
-  { id: "osh", data: [{ street: "test osh street", phone: "test osh phone" }] },
+  {
+    id: "osh",
+    data: [
+      {
+        street:
+          "г. Ош, ул. А. Шакиров, 275/364. ( ор-р. 'Нурзаман', ТЦ 'Глобус') ",
+        phone: "+996(700) 00 51 50 ",
+        title: "Ош",
+      },
+      {
+        street: "Г. Ноокат, ул. Каленина (ор-р Налоговая) ",
+        phone: "+996700135150  +996550135150 ",
+      },
+      {
+        street: "Г. Кара-Суу, ул. Бакиева (ор-р Комплекс «Турак-Жай») ",
+        phone: "+996227015150  +996557015150 ",
+      },
+      {
+        street:
+          "р-н Кара-Кулжа, ул. Калматаева 72 (магазин «Токтосун-Ата» 2-этаж) ",
+        phone: "+996773144437   +996551730471 ",
+      },
+      {
+        street: "с. Гулчо (Алай), ул. Нарматова 138 ",
+        phone: "+996990040088    +996220040088 +996700040088  ",
+      },
+      {
+        street: "г. Узген, ул. Манас-Ата, БЦ «Озгон ордо» 1-этаж) ",
+        phone: "+996708875050 +996220040088 +996700040088  ",
+      },
+    ],
+  },
   {
     id: "chui",
-    data: [{ street: "test issyk street", phone: "test issyk phone" }],
+    data: [
+      {
+        street: "г. Бишкек, Чуй/ Кулиева 20, БЦ “Жан-Сали”, 5-этаж. ",
+        phone: " +996(700) 00 51 51",
+        title: "Чуй",
+      },
+      {
+        street: "г. Бишкек, Чуй/ Кулиева 20, БЦ “Жан-Сали”, 5-этаж.   ",
+        phone: " 0700-005-151",
+      },
+      {
+        street:
+          "г. Бишкек, рынок Дордой, Мурас спорт, 14-проход(ЛЭП), контейнер 25 ",
+        phone: "+996(509)525200 ",
+      },
+      {
+        street: "г.Токмок ул Советская 106 А ",
+        phone: "0707 330-968 ",
+      },
+    ],
   },
   {
     id: "talas",
-    data: [{ street: "test osh street", phone: "test osh phone" }],
+    data: [
+      {
+        street: "г. Талас, ул. Сарыгулова 59А ",
+        phone: "+996704445151 ",
+        title: "Талас",
+      },
+    ],
   },
   {
     id: "jalal-abad",
-    data: [{ street: "test issyk street", phone: "test issyk phone" }],
+    data: [
+      {
+        street: " г. Жалал-Абад, ул. Токтогул, 34, БЦ 'Plaza'",
+        phone: "+996700700857",
+        title: "Жалал-Абад",
+      },
+      {
+        street: "г.Таш-Комур, ул. Токтогула 2,  ",
+        phone: "+996774360005  +996704360005 ",
+      },
+    ],
   },
   {
     id: "batken",
-    data: [{ street: "test osh street", phone: "test osh phone" }],
+    data: [
+      {
+        street:
+          "Баткенская обл .,г. Кызыл -Кыя ул.Асаналиева ориентир “Банк Азия” -2 этаж ",
+        phone: "+996222445151 ",
+        title: "Баткен",
+      },
+    ],
   },
   {
     id: "naryn",
-    data: [{ street: "test osh street", phone: "test osh phone" }],
+    data: [
+      {
+        street: "г. Нарын, ул. Ленина 41К, ТЦ 'Нарын Сити', 2-этаж ",
+        phone: " +996(702) 28 51 51 ",
+        title: "Нарын",
+      },
+      {
+        street: "г. Кочкор-Ата, ул. Ленина 42/3 (ор-р ОАО Кыргызнефтгаз) ",
+        phone: "+996708661153",
+      },
+    ],
+  },
+];
+
+const rusData = [
+  {
+    id: "russian",
+    street:
+      "Г. Москва метро Новые Черёмушки; улица Профсоюзная, 56. Бизнес Центр «Cherry Tower», этаж 17, офис №29  ",
+    phone: "+74951362208  +79685547788  ",
+    title: "Россия",
   },
 ];
 
@@ -62,6 +177,12 @@ function MainMap() {
   const [selectRegion, setSelectedRegion] = useState(options[0]);
 
   const [modal, setModal] = useState(false);
+  const [rusModal, setRusModal] = useState(false);
+
+  let domNode = useClickOutside(() => {
+    setModal(false);
+    setRusModal(false);
+  });
 
   const getLocationName = (event) => {
     return event.target.attributes.name.value;
@@ -88,7 +209,7 @@ function MainMap() {
         </div>
       </div>
 
-      <div className={styles.kg_map_info}>
+      <div ref={domNode} className={styles.kg_map_info}>
         <Image src={kg} />
         <div className={styles.country}>
           <span>Кыргызстан </span>
@@ -108,36 +229,53 @@ function MainMap() {
             <h1 style={{ textAlign: "center" }}>
               {selectRegion.data[0].title}
             </h1>
-            <h1 style={{ textAlign: "center" }}>{selectRegion.id}</h1>
+            {/* <h1 style={{ textAlign: "center" }}>{selectRegion.id}</h1> */}
             <div>
               {selectRegion.data.map((item) => (
                 <div className={styles.info_kg}>
-                  <div className={styles.street_info}>${item.street}</div>
-                  <div className={styles.phone_info}>${item.phone}</div>
+                  <div className={styles.street_info}>{item.street}</div>
+                  <div className={styles.phone_info}>{item.phone}</div>
                 </div>
               ))}
             </div>
-            <button
-              className={styles.close_btn}
-              onClick={() => setModal(false)}
-            >
-              close
-            </button>
           </div>
         )}
-      </div>
-
-      <div className={styles.russian_map}>
-        <div className={styles.rus_map_info}>
-          <Image src={rus} />
-          <div className={styles.country}>
-            <span>Россия</span>
+        <div
+          className={styles.russian_map}
+          onClick={() => {
+            setRusModal(true);
+          }}
+        >
+          <div>
+            <div ref={domNode} className={styles.rus_map_info}>
+              <Image src={rus} />
+              <div className={styles.country}>
+                <span>Россия</span>
+              </div>
+            </div>
+            <div>
+              <Image src={ru} />
+            </div>
           </div>
         </div>
-        <div>
-          <Image src={ru} />
-        </div>
       </div>
+
+      {rusModal && (
+        <div className={styles.info_contacts}>
+          <h1 style={{ textAlign: "center" }}>{rusData[0].title}</h1>
+          {/* <h1 style={{ textAlign: "center" }}>{selectRegion.id}</h1> */}
+          <div>
+            {rusData.map((item) => (
+              <div className={styles.info_kg}>
+                <div className={styles.street_info}>{item.street}</div>
+                <div className={styles.phone_info}>{item.phone}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+      <Footer />
     </>
   );
 }
--- pages/map.js
+++ pages/map.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Footer from "../components/Footer";
 import MainMap from "../components/Map/MainMap";
+import Navbar from "../components/Navbar";
 
 export default function map() {
   return (
     <main>
+      <Navbar />
       <MainMap />
     </main>
   );
